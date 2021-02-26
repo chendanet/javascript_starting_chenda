403838df7efcf7ae70ea464f0496877c5ea1fe3d
chendanet <chenda_net@yahoo.fr>
2021-02-26 02:26:41 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Welcome to the fundamentals of Javascript !
+You will find here several files of JS script to call on the index.html
---
+++ script_1.js
@@ -0,0 +1,10 @@
+
+console.log("Bonjour, monde !");
+
+/*2.1.1. Bonjour*/
+
+
+let askName= prompt("Quel est ton joli prénom ?")
+console.log(`Bonjour ${AskName} !`)
+
+/*2.1.2. Salut*/
